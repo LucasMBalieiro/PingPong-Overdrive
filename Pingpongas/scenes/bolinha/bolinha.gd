589384a9dfa485f7e308caa21d82c0f17fd43db7
Lucas Balieiro <lucas.macbali@gmail.com>
2024-04-29 07:19:12 -0300

    Hit collision

---
+++ Pingpongas/scenes/bolinha/bolinha.gd
@@ -0,0 +1,68 @@
+extends Area2D
+
+var velo_bolinha: int = 200
+var posicao_saque: Vector2 = Vector2(736, 632)
+var direcao: Vector2 = Vector2(0, 0)
+
+#limites da mesa
+var x_min: int = 440
+var x_max: int = 840
+
+#ultimo que rebateu, usado pra determinar quem "marcou"
+var player1_rebate : bool
+
+
+func _ready():
+	pos_saque() 
+
+func _process(delta):
+	move_bola(delta)
+	colisao()
+
+
+
+
+func move_bola(delta):
+	position += direcao * velo_bolinha * delta
+
+#TODO: fazer reset de posição depois de pontuar
+func pos_saque():
+	position = posicao_saque
+
+func colisao():
+	if position.x <= x_min or position.x >= x_max:
+		if position.y > 120 and position.y < 600:
+			if player1_rebate:
+				velo_bolinha = 200
+				direcao = Vector2(0, 0)
+				position = Vector2(736, 632)
+			else:
+				velo_bolinha = 200
+				direcao = Vector2(0, 0)
+				position = Vector2(544, 88)
+	
+	if position.y <= 40 or position.y >= 680:
+		if player1_rebate:
+			velo_bolinha = 200
+			direcao = Vector2(0, 0)
+			position = Vector2(544, 88)
+		else:
+			velo_bolinha = 200
+			direcao = Vector2(0, 0)
+			position = Vector2(736, 632)
+
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Jogadores"):
+		var player_position = body.global_position
+		var ball_position = global_position
+		var collision_normal = (ball_position - player_position).normalized()
+		velo_bolinha = velo_bolinha * 1.1
+		
+		direcao = collision_normal
+		
+		if body.jogador1:
+			player1_rebate = true
+		else:
+			player1_rebate = false
--- Pingpongas/scenes/jogador/jogador.gd
+++ Pingpongas/scenes/jogador/jogador.gd
@@ -1,10 +1,14 @@
 extends StaticBody2D
-
 @export var jogador1: bool
-var velocidade: int = 300 
+var velo_player: int = 300
+var hit_cooldown: float = 1.0  # Cooldown time in seconds
+var last_hit_time: float = 0.0  # Time when the last hit occurred
+
+var collision_shape: CollisionShape2D
 
 func _ready():
-	pass 
+	collision_shape = $CollisionShape2D
+	collision_shape.disabled = true 
 
 
 func _process(delta):
@@ -15,19 +19,28 @@ func _process(delta):
 func move_jogador(delta):
 	if jogador1:
 		if  Input.is_action_pressed("left-1"):
-			position.x -= velocidade * delta
+			position.x -= velo_player * delta
 		elif  Input.is_action_pressed("right-1"):
-			position.x += velocidade * delta
-		elif  Input.is_action_pressed("hit-1"):
-			pass
+			position.x += velo_player * delta
+		elif  Input.is_action_pressed("hit-1") and can_hit():
+			collision_shape.disabled = false
+			last_hit_time = Time.get_ticks_msec() / 1000.0 
+		elif not Input.is_action_pressed("hit-1"):
+			collision_shape.disabled = true
 	else:
 		if  Input.is_action_pressed("left-2"):
-			position.x -= velocidade * delta
+			position.x -= velo_player * delta
 		elif  Input.is_action_pressed("right-2"):
-			position.x += velocidade * delta
-		elif  Input.is_action_pressed("hit-2"):
-			pass
+			position.x += velo_player * delta
+		elif  Input.is_action_pressed("hit-2") and can_hit():
+			collision_shape.disabled = false
+			last_hit_time = Time.get_ticks_msec() / 1000.0  # Record the time of the hit
+		elif not Input.is_action_pressed("hit-2"):
+			collision_shape.disabled = true
 
 
 func limit_position():
-	position.x = clamp(position.x,440,840)
+	position.x = clamp(position.x,400,880)
+
+func can_hit():
+	return Time.get_ticks_msec() / 1000.0 - last_hit_time >= hit_cooldown
